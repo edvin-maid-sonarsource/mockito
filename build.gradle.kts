c9ad64b849fb7be5c9c1167dc29cbb69423232ee
Edvin Maid <edvin.maid@sonarsource.com>
2026-01-28 14:53:11 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,15 @@ plugins {
     id("eclipse")
     id("com.github.ben-manes.versions") version "0.51.0"
     id("mockito.root.releasing-conventions")
+    id("org.sonarqube") version "7.1.0.6387"
 
     // Top-level android plugin declaration required for :mockito-integration-tests:android-tests to work
     alias(libs.plugins.android.application) apply false
 }
 
-
+sonar {
+  properties {
+    property("sonar.projectKey", "sonarsourceresearch_mockito-test")
+    property("sonar.organization", "sonarsourceresearch")
+  }
+}
